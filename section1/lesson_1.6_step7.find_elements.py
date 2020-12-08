92e80e6b33adf15952ac43fda9e2272246e67ac0
bohdan <gavrilenkobg@gmail.com>
2020-12-08 22:42:19 +0200

    Added step7

--- section1/lesson_1.6_step7.find_elements.py
+++ section1/lesson_1.6_step7.find_elements.py
@@ -1,3 +1,16 @@
 from selenium import webdriver
 import time
 
+browser = webdriver.Chrome()
+try:
+    browser.get("http://suninjuly.github.io/huge_form.html")
+    elements = browser.find_elements_by_tag_name("input")
+    for element in elements:
+        element.send_keys("Hello")
+
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+finally:
+    time.sleep(30)
+    browser.quit()
